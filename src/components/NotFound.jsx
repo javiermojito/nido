026ce694d425338abc465e3b344d3ebbf9256e0f
Javier Collazos <jcollazosgomez@gmail.com>
2021-01-08 23:23:20 -0500

    Stylish NotFound component

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import "./css/NotFound.css";
 
 const NotFound = () => {
-  return <h1>404 NOT FOUND</h1>;
+  return (
+    <div className="info-container">
+      <div className="image-left-container">
+        <img src="" alt="" />
+      </div>
+      <div className="info-right-container">
+        <h2>404 NOT FOUND</h2>
+        <p>
+          El recurso al que intentas acceder...
+          <br /> al parecer...
+          <br /> <b>no existe.</b>
+        </p>
+        Illustration by <a href="http://www.saramaese.com/">Sara Maese</a> from{" "}
+        <a href="https://icons8.com/">Icons8</a>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
