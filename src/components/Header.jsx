4a637b146904c55d30972489c4d00465e0fdf35b
Javier Collazos <jcollazosgomez@gmail.com>
2021-01-12 23:33:32 -0500

    NotFound component responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
         <Link to="/"> <img className="w-1/7 h-12 ml-4" src="https://i.imgur.com/v3hO1M0.png"alt="nido logo"/> </Link>
         <div className="h-full my-auto">
           <ul className="headerTailwind flex flex-row content-center">
-            <li className="pr-3 text-sm md:pr-5 md:text-md hover:underline "><Link to="/inicio">Inicio</Link></li>
+            <li className="pr-3 text-sm md:pr-5 md:text-md hover:underline"><Link to="/inicio">Inicio</Link></li>
             <li className="pr-3 text-sm md:pr-5 md:text-md hover:underline"><Link to="/cursos">Cursos</Link></li>
             <li className="pr-3 text-sm md:pr-5 md:text-md hover:underline"><Link to="/contacto">Contacto</Link></li>
           </ul>
--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,21 +1,20 @@
 import React from "react";
-import "./css/NotFound.css";
 
 const NotFound = () => {
   return (
-    <div className="info-container">
-      <div className="image-left-container">
-        <img src="" alt="" />
+    <div className="flex flex-col sm:flex-col md:flex-row mx-auto md:ml-auto md:gap-x-0 md:justify-center">
+      <div className="md:mx-0 w-auto md:w-1/2 lg:w-1/3">
+        <img src="https://i.imgur.com/RA1pMaW.png" alt="404 NOT FOUND IMAGE" />
       </div>
-      <div className="info-right-container">
-        <h2>404 NOT FOUND</h2>
-        <p>
-          El recurso al que intentas acceder...
-          <br /> al parecer...
-          <br /> <b>no existe.</b>
-        </p>
-        Illustration by <a href="http://www.saramaese.com/">Sara Maese</a> from{" "}
-        <a href="https://icons8.com/">Icons8</a>
+      <div className="flex flex-col md:my-auto md:ml-0 md:w-96" >
+        <div className="text-normal text-center	text-3xl md:text-4xl md:text-left lg:text-4xl font-bold">404 NOT FOUND</div>
+        <div className="text-center text-2xl pl-8 pr-8 pt-4 pb-3 md:text-left md:pl-0">
+          El recurso al que intentas acceder, al parecer, no existe.
+        </div>
+        <div className="text-center	text-normal md:text-left">
+          Illustration by <a className="font-bold" href="http://www.saramaese.com/">Sara Maese</a> from{" "}
+          <a className="font-bold" href="https://icons8.com/">Icons8</a>
+        </div>
       </div>
     </div>
   );
