10802bf70ea48dde9553abf380e3c148fb440309
Javier Collazos <jcollazosgomez@gmail.com>
2021-01-14 21:43:05 -0500

    Adding fontawesome to the project

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -7,6 +7,13 @@ const Content = () => {
 
   return (
     <div className="flex flex-wrap justify-center mt-5 mb-5">
+      {/* <div>
+      <div>Objeto Virutal de Aprendizaje (OVA)</div>
+      <div>
+        <div>4 Unidades - 25 temas</div>
+        <div>20 minutos</div>
+      </div>
+    </div> */}
       <div className="w-4/5 h-64 lg:h-screen">
         <a
           className="lg:hidden cursor-pointer"
@@ -14,7 +21,7 @@ const Content = () => {
         >
           {" "}
           {/* IMPORTANTE CAMBIAR EL ENLACE  TODO*/}
-          <div className="rounded-xl bg-gradient-to-br from-purple-600 via-purple-500 to-purple-400 w-full h-full flex flex-wrap content-center shadow-md  lg:shadow-none">
+          <div className="rounded-xl bg-gradient-to-br from-purple-600 via-purple-500 to-purple-400 w-full h-full flex flex-wrap content-center shadow-md  lg:shadow-none transition duration-400 ease-in-out transform hover:-translate-y-0.5">
             <img
               className="mx-auto"
               src="https://i.imgur.com/h6QYw4a.png"
