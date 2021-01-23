139347e867c3d1b389d9f5d8c2dcbe409f60b7d9
Javier Collazos <jcollazosgomez@gmail.com>
2021-01-23 15:39:42 -0500

    Last section complete

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,9 +8,13 @@ const Home = () => {
       {/* // Landing // */}
       <div className="header-home w-full h-96 pt-10 pl-7 pr-10 mb-0">
         <div className="">
-          <h1 className="font-poppins font-semibold text-4xl tracking-tighter">Cursos gratuitos de diseño</h1>
-          <p className="font-popping text-2xl tracking-tighter leading-7">Aprende con recursos digitales a tu alcance</p>
-          <br/>
+          <h1 className="font-poppins font-semibold text-4xl tracking-tighter">
+            Cursos gratuitos de diseño
+          </h1>
+          <p className="font-popping text-2xl tracking-tighter leading-7">
+            Aprende con recursos digitales a tu alcance
+          </p>
+          <br />
           <Link to="/cursos">
             <div className="w-max h-auto bg-greenNido pl-4 pr-4 pt-2 pb-2 font-semibold font-poppins border-solid border-2 border-black botder-opacity-100">
               Ver Cursos
@@ -21,53 +25,87 @@ const Home = () => {
 
       {/* // Feature -> sin costo // */}
       <div className="w-full flex flex-col bg-redNido col-1 h-128 pt-11 mt-0">
-          <div className="h-3/5 w-auto mx-auto inline-block align-middle ">
-            <img className="h-full" src="https://blush.design/api/download?shareUri=Qh8dZLB1u&s=0%7Ef7d3bb&w=800&h=800&fm=png" alt="Feature sin costo"/>
-          </div>
-          <div className="h-2/6 pl-6 pr-6 text-white text-center tracking-tight flex flex-col col-1 content-center justify-center">
-            <h2 className="font-poppins text-2xl font-semibold">Sin costo, ni registros</h2>
-            <br/>
-            <p className="font-roboto text-lg leading-6">Aprende algo nuevo sin aceptar planes de dudosa beneficiencia</p>
-          </div>
+        <div className="h-3/5 w-auto mx-auto inline-block align-middle ">
+          <img
+            className="h-full"
+            src="https://blush.design/api/download?shareUri=Qh8dZLB1u&s=0%7Ef7d3bb&w=800&h=800&fm=png"
+            alt="Feature sin costo"
+          />
+        </div>
+        <div className="h-2/6 pl-6 pr-6 text-white text-center tracking-tight flex flex-col col-1 content-center justify-center">
+          <h2 className="font-poppins text-2xl font-semibold">
+            Sin costo, ni registros
+          </h2>
+          <br />
+          <p className="font-roboto text-lg leading-6">
+            Aprende algo nuevo sin aceptar planes de dudosa beneficiencia
+          </p>
+        </div>
       </div>
 
       {/* //Feature -> Sin conexión// */}
       <div className="w-full flex flex-col col-1 h-128 bg-white pt-14">
-          <div className="h-6/7 w-full inline-block align-middle">
-            <img className="pl-5 pr-5 w-auto mx-auto" src="https://blush.design/api/download?shareUri=aPwzY7odD&s=0.2%7E8d574d&w=800&h=800&fm=png" alt="Feature sin conexión"/>
-          </div>
-          <div className="h-full pl-6 pr-6 pb-4 text-black text-center tracking-tight flex flex-col col-1 content-center justify-center ">
-            <h2 className="font-poppins text-2xl font-semibold leading-6">Úsalos sin conexión a internet</h2>
-            <br/>
-            <p className="font-roboto text-lg leading-6">Descárgalo en tu computadora con Windows o Linux y úsalos en tu próximo viaje, cafetería favorita, tú decides.</p>
-          </div>
+        <div className="h-6/7 w-full inline-block align-middle">
+          <img
+            className="pl-5 pr-5 w-auto mx-auto"
+            src="https://blush.design/api/download?shareUri=aPwzY7odD&s=0.2%7E8d574d&w=800&h=800&fm=png"
+            alt="Feature sin conexión"
+          />
+        </div>
+        <div className="h-full pl-6 pr-6 pb-4 text-black text-center tracking-tight flex flex-col col-1 content-center justify-center ">
+          <h2 className="font-poppins text-2xl font-semibold leading-6">
+            Úsalos sin conexión a internet
+          </h2>
+          <br />
+          <p className="font-roboto text-lg leading-6">
+            Descárgalo en tu computadora con Windows o Linux y úsalos en tu
+            próximo viaje, cafetería favorita, tú decides.
+          </p>
+        </div>
       </div>
 
       {/* //Feature -> Responsive design// */}
       <div className="w-full flex flex-col col-1 h-128 bg-blueNido pt-10 pb-7">
-          <div className="h-3/5 w-auto mx-auto inline-block align-middle ">
-            <img className="h-full mx-auto" src="https://blush.design/api/download?shareUri=XtWpTVm_Y&s=0.1%7E583318&w=800&h=800&fm=png" alt="Feature Resposive Design"/>
-          </div>
-          <div className="h-2/6 pl-5 pr-5 pt-6 text-white text-center tracking-tight  flex flex-col col-1 content-center justify-center ">
-            <h2 className="font-poppins text-2xl font-semibold leading-6">Accede desde cualquier dispositivo</h2>
-            <br/>
-            <p className="font-roboto text-lg leading-6">No importa si estas en tu celular, computadora o nevera con pantalla. Podrás disfrutar del contenido de nuestros cursos.</p>
-          </div>
+        <div className="h-3/5 w-auto mx-auto inline-block align-middle ">
+          <img
+            className="h-full mx-auto"
+            src="https://blush.design/api/download?shareUri=XtWpTVm_Y&s=0.1%7E583318&w=800&h=800&fm=png"
+            alt="Feature Resposive Design"
+          />
+        </div>
+        <div className="h-2/6 pl-5 pr-5 pt-6 text-white text-center tracking-tight  flex flex-col col-1 content-center justify-center ">
+          <h2 className="font-poppins text-2xl font-semibold leading-6">
+            Accede desde cualquier dispositivo
+          </h2>
+          <br />
+          <p className="font-roboto text-lg leading-6">
+            No importa si estas en tu celular, computadora o nevera con
+            pantalla. Podrás disfrutar del contenido de nuestros cursos.
+          </p>
+        </div>
       </div>
 
       {/* // CTA //*/}
-      <div className="flex flex-wrap content-center justify-center bg-purpleNido gap-y-8 pt-16 pb-6">
-        <h2 className="font-poppins text-white text-2xl font-semibold text-center">Vive la experiencia de los Objetos Virtuales de Aprendizaje</h2>
-        <Link to="/cursos">
-            <div className="w-full h-auto mx-auto bg-greenNido pl-4 pr-4 pt-2 pb-2 font-semibold font-poppins border-solid border-2 border-black botder-opacity-100">
+      <div className="bg-purpleNido">
+        <div className="flex flex-wrap content-center justify-center bg-purpleNido gap-y-8 pt-16 pb-6 mx-auto sm:flex-col sm:col-1 sm:w-128">
+          <h2 className="font-poppins text-white text-2xl font-semibold text-center sm:text-3xl lg:text-4xl">
+            Vive la experiencia de los Objetos Virtuales de Aprendizaje
+          </h2>
+          <Link to="/cursos">
+            <div className="w-max h-auto mx-auto bg-greenNido pl-4 pr-4 pt-2 pb-2 font-semibold font-poppins border-solid border-2 border-black botder-opacity-100 sm:text-lg">
               Ver Cursos
             </div>
           </Link>
+        </div>
       </div>
 
       {/* //CTA image// */}
       <div className="bg-purpleNido h-36 mb-32 content-start  flex flex-wrap content-start justify-center">
-        <img className="h-64" src="https://blush.design/api/download?shareUri=WXOD80qqd&s=0%7E8d574d&w=800&h=800&fm=png" alt="Peace sign"/>
+        <img
+          className="h-64"
+          src="https://blush.design/api/download?shareUri=WXOD80qqd&s=0%7E8d574d&w=800&h=800&fm=png"
+          alt="Peace sign"
+        />
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -47,6 +47,16 @@ module.exports = {
         "160": "40rem",
         "180": "45rem",
       }),
+      width: (theme) => ({
+        "screen/2": "50vh",
+        "screen/3": "calc(100vh / 3)",
+        "screen/4": "calc(100vh / 4)",
+        "screen/5": "calc(100vh / 5)",
+        "128": "32rem",
+        "144": "36rem",
+        "160": "40rem",
+        "180": "45rem",
+      }),
     },
   },
   variants: {
