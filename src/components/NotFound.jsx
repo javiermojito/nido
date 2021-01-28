a33ae05f5ec87cc1ae085319ac134880606e1eb8
Javier Collazos <jcollazosgomez@gmail.com>
2021-01-27 23:17:57 -0500

    Not found component renewed

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -2,18 +2,23 @@ import React from "react";
 
 const NotFound = () => {
   return (
-    <div className="flex flex-col sm:flex-col md:flex-row mx-auto md:ml-auto md:gap-x-0 md:justify-center">
-      <div className="md:mx-0 w-auto md:w-1/2 lg:w-1/3">
-        <img src="https://i.imgur.com/RA1pMaW.png" alt="404 NOT FOUND IMAGE" />
+    <div className="flex flex-col sm:flex-col md:flex-row mx-auto md:ml-auto md:gap-x-0 md:justify-center h-screen  bg-purpleNido text-white flex justify-center content-center">
+      <div className="md:mx-0 md:w-1/2 lg:w-1/3 flex flex-wrap content-center justify-center xxl:justify-end xxl:pr-12">
+        <img
+          className="h-72 lg:h-80 xl:h-96"
+          src="https://blush.design/api/download?shareUri=ISAlVtb8a&s=0%7Ef7d3bb&w=800&h=800&fm=png"
+          alt="404 NOT FOUND IMAGE"
+        />
       </div>
-      <div className="flex flex-col md:my-auto md:ml-0 md:w-96" >
-        <div className="text-normal text-center	text-3xl md:text-4xl md:text-left lg:text-4xl font-extrabold">404 NOT FOUND</div>
-        <div className="text-center text-2xl pl-8 pr-8 pt-4 pb-3 md:text-left md:pl-0">
-          El recurso al que intentas acceder, al parecer, <span className="font-bold">no existe.</span> 
-        </div>
-        <div className="text-center	text-normal md:text-left">
-          Illustration by <a className="font-bold" href="http://www.saramaese.com/">Sara Maese</a> from{" "}
-          <a className="font-bold" href="https://icons8.com/">Icons8</a>
+      <div className="flex justify-center content-center xxl:content-left xxl:justify-left">
+        <div className="flex flex-col md:my-auto md:ml-0 md:w-96">
+          <div className="text-normal text-center	text-3xl md:text-4xl md:text-left lg:text-4xl font-extrabold">
+            404 NOT FOUND
+          </div>
+          <div className="text-center text-2xl pl-8 pr-8 pt-4 pb-3 md:text-left md:pl-0">
+            El recurso al que intentas acceder, al parecer,{" "}
+            <span className="font-bold">no existe.</span>
+          </div>
         </div>
       </div>
     </div>
